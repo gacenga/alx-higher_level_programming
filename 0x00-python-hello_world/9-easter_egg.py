3f3900f4182172458378c02529bd50ef013cf1a0
gacenga <jimwillymurigu@gmail.com>
2024-01-02 05:37:04 -0800

    9-easter_egg.py-prints “The Zen of Python”, by TimPeters

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-import this
-english_part = this.s[:this.s.find("Gur Mra bs Clguba")].strip()
-print(english_part, end='')
+import this;print(this.s[:this.s.find("Gur")].strip(),end='')
