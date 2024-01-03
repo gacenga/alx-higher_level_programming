525632e444d16fcf88f2139b78f440dacf6605b4
gacenga <jimwillymurigu@gmail.com>
2024-01-03 06:04:53 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alpha in range(ord('z'), ord('a') -1, -1):
+for alpha in range(ord('z'), ord('a') - 1, - 1):
     if (alpha % 2) == 0:
         newalpha = chr(alpha)
     elif (alpha % 2) != 0:
