c812a29a625c064be7fc234d2202dd9f92c1b450
gacenga <jimwillymurigu@gmail.com>
2024-01-04 02:33:16 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv
+    num = len(arguments) - 1
+    if num == 0:
+        print("{} arguments.".format(num))
+    elif num > 0:
+        print("{} arguments:".format(num))
+        for i in range(1, num + 1):
+            print(f"{i}: {arguments[i]}")
