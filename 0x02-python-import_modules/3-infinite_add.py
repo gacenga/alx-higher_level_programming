561770f2de802eaf0cabbddb81170498776cf511
gacenga <jimwillymurigu@gmail.com>
2024-01-04 02:45:05 -0800

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argument = sys.argv
+    num = 0
+    numarg = len(argument) -1
+    for i in range(1, numarg + 1):
+        num += int(argument[i])
+    print(num)
