5dddacacdd73d8eede18f7af40c74b1c1a7d950d
gacenga <jimwillymurigu@gmail.com>
2024-01-04 02:46:42 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argument = sys.argv
     num = 0
-    numarg = len(argument) -1
+    numarg = len(argument) - 1
     for i in range(1, numarg + 1):
         num += int(argument[i])
     print(num)
