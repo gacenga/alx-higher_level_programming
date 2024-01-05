fae659209bff36495ef1ab15c4e4f8eee33688ee
gacenga <jimwillymurigu@gmail.com>
2024-01-05 04:45:09 -0800

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    number = len(my_list)
+    if idx < 0 or idx > number:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
