4aee03942db6b054ba141d356b9e769ddb319181
gacenga <jimwillymurigu@gmail.com>
2024-01-05 06:04:22 -0800

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for i in range(0, cols):
+        for r in range(0, rows):
+            if r != rows - 1:
+                print("{}".format(matrix[i][r]), end=" ")
+            if r == rows - 1:
+                print("{}".format(matrix[i][r]), end="\n")
