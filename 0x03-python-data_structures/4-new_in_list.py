7024c0f219591ac6147539104555fce92a74b7b5
gacenga <jimwillymurigu@gmail.com>
2024-01-06 00:39:31 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     number = len(my_list)
     new_list = my_list[:]
-    if idx < 0 or idx > number:
+    if idx < 0 or idx >= number:
         return my_list
     else:
         new_list[idx] = element
