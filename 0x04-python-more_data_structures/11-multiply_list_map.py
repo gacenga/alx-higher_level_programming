ec2ea07fb1a70b2839efd8b3418bcf4ba2c1f225
gacenga <jimwillymurigu@gmail.com>
2024-01-10 14:11:11 -0800

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = []
-    for i in range(0, len(my_list)):
-        new_list.append(my_list[i] * number)
+    new_list = list(map(lambda x: x * number, my_list))
     return new_list
