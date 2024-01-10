ee525b046d5c331f958023e48b7fbacf37405b43
gacenga <jimwillymurigu@gmail.com>
2024-01-10 13:52:17 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        new_dict = a_dictionary.copy()
-        sorted_stuff = sorted(a_dictionary.items(), key=lambda x: x[1])
-        newest_dict = dict(sorted_stuff)
-        last_key = list(newest_dict.keys())[-1]
-        last_value = newest_dict[last_key]
-        return last_value
+        best = max(a_dictionary, key=a_dictionary.get)
+        return best
     else:
         return None
