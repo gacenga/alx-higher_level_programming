c1b3d9352f2ad06bfdf0318fd443657349b25611
gacenga <jimwillymurigu@gmail.com>
2024-02-19 01:48:26 -0800

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,6 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("wrong type")
             except IndexError:
                 print("out of range")
-                break
     finally:
         return my_list_3
