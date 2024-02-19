3bc6f71a623ca232a0aa51b8778df36aa44d05c3
gacenga <jimwillymurigu@gmail.com>
2024-02-19 01:05:51 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,9 +6,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, ArithmeticError, ValueError, TypeError):
         pass
     finally:
-        if x:
-            print("Inside result: {}".format(x), end="\n")
-            return x
-        else:
-            print("Inside result: {}".format("None"), end="\n")
-            return None
+        print("Inside result: {}".format(x), end="\n")
+        return x
