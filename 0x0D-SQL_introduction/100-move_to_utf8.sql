e9c656abea41969a53847379dff66c924d313c9c
gacenga <jimwillymurigu@gmail.com>
2024-03-13 01:23:58 -0700

    correction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,6 @@
 -- choose database
-USE hbtn_0c_0;
+USE hbtn_0c_0
 
--- converts hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- converts first_table to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- coverts field name in first_table to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- converts database to UTF8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- number of records with the same score in the table second_table
 SELECT score, COUNT(*) AS number
 FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY number DESC
