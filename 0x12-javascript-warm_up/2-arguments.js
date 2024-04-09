cd55a96dec7fb45ee6656fc236e8e9d8a177631e
gacenga <jimwillymurigu@gmail.com>
2024-04-09 00:52:58 -0700

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-let myVar = process.argv;
-let x = myVar.length;
-if (x > 3) {
+if (process.argv.length > 3) {
     console.log('Arguments found');
-} else if (x === 3) {
+} else if (process.argv.length === 3) {
     console.log('Argument found');
 } else {
     console.log('No argument');
 }
-
